4041d731048cef78a6bd4f5a4827e29906dea574
timonbarth <timonnicholas.barth@gmail.com>
2019-08-30 15:08:09 +0200

    2 files modified

--- src/app/rechner-rotation/rechner-rotation.component.ts
+++ src/app/rechner-rotation/rechner-rotation.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class RechnerRotationComponent implements OnInit {
 
   constructor() { }
-
+  
+  rechne() {
+   
+  }
   ngOnInit() {
   }
 
