c002de13a2a961349b546160c104645808bfb5cf
timonbarth <timonnicholas.barth@gmail.com>
2019-08-30 15:04:24 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListenComponent } from './listen/listen.component';
+import { RechnerRotationComponent } from './rechner-rotation/rechner-rotation.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ListenComponent ],
+  declarations: [ AppComponent, HelloComponent, ListenComponent, RechnerRotationComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/rechner-rotation/rechner-rotation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rechner-rotation',
+  templateUrl: './rechner-rotation.component.html',
+  styleUrls: ['./rechner-rotation.component.css']
+})
+export class RechnerRotationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
