89853265236e5b2ff2188a7c6b7de27d1017a487
timonbarth <timonnicholas.barth@gmail.com>
2019-10-22 11:55:23 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListenComponent } from './listen/listen.component';
 import { RechnerRotationComponent } from './rechner-rotation/rechner-rotation.component';
+import { ModuloComponent } from './modulo/modulo.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ListenComponent, RechnerRotationComponent ],
+  declarations: [ AppComponent, HelloComponent, ListenComponent, RechnerRotationComponent, ModuloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
