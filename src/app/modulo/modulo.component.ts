a9359099cb2f6b765aa630d9dd3d83385cbea5bd
timonbarth <timonnicholas.barth@gmail.com>
2019-10-22 12:12:49 +0200

    1 file modified

--- src/app/modulo/modulo.component.ts
+++ src/app/modulo/modulo.component.ts
@@ -12,13 +12,12 @@ ergebnis = 'noch nix';
   zahl1 = 10;
   zahl2 = 20;
   zwischenergebnis = 0;
-  summe = 0
   ngOnInit() {
   }
 rechne() {
     this.zwischenergebnis = this.zahl1/this.zahl2;
-    let summe = this.zwischenergebnis*this.zahl2;
-    this.ergebnis = this.summe.toString();
+    let summe = this.zwischenergebnis*this.zahl2
+    this.ergebnis = summe.toString();
 
   }
 }
